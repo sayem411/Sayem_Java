53e6c3f3c3a38116be329250af86c5714997991f
Almas Sayem <mdassayem0@gmail.com>
2026-09-19 00:12:06 +0600

    updated

---
+++ math.java
@@ -0,0 +1,10 @@
+public class math {
+    public static void main(String[] args) {
+        System.out.println(Math.max(5,6));
+        System.out.println(Math.min(5,6));
+
+        //Random
+        System.out.println((int)(Math.random()*1000));
+
+    }
+}
