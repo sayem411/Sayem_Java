258775e244800da938e34c016cf029d46ca8c81a
Almas Sayem <mdassayem0@gmail.com>
2026-09-18 23:44:26 +0600

    updated

---
+++ constants.java
@@ -0,0 +1,13 @@
+public class constants {
+    public static void main(String[] args) {
+       //constants
+       int age = 30;
+       age=31;
+       age=32;
+
+    final float PI=3.14F;
+
+    System.out.println(age);
+    System.out.println(PI);
+    }
+}
---
+++ operators.java
@@ -0,0 +1,21 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=1;
+        int b =2;
+        int sum = a+b;
+        int diff = a-b;
+        int mul = a*b;
+        int div = a/b;
+        int mod = a%b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
+
+        double c=5;
+        double d =3;
+        double modulo=c%d;
+        System.out.println(modulo);
+    }
+}
