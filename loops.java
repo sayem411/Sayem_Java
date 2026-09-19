042f3e2a6fc6207d8cb555962e06fe89fec2a5e2
Almas Sayem <mdassayem0@gmail.com>
2026-09-20 00:35:44 +0600

    updated

--- loops.java
+++ loops.java
@@ -1,5 +1,25 @@
 public class loops {
     public static void main(String[] args) {
-        
+        System.out.println("For");
+        //for loop
+        for(int i=100;i>=1;i--){
+            System.out.println(i);
+        }
+System.out.println("While");
+       //while loop
+       int j=100;
+       while(j>=1){
+        System.out.println(j);
+       j--;
+       } 
+System.out.println(" Do While");
+
+int k=100;
+do{
+    System.out.println(k);
+    k--;
+}
+while(k>=1);
+
     }
 }
