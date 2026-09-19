11e1ceff4fbf41dfcbd2c28859e2e34b283714b3
Almas Sayem <mdassayem0@gmail.com>
2026-09-19 22:04:59 +0600

    updated

--- comparison_operator.java
+++ comparison_operator.java
@@ -1,5 +1,21 @@
 public class comparison_operator {
     public static void main(String[] args) {
-        
+        /*
+        Comparison Operator
+        a==b
+        a!=b
+        a<b
+        a>b
+        a<=b
+        a>=b
+        */
+
+        //conditional Statement
+        boolean isSunUp=false;
+        if(isSunUp==true)
+            System.out.println("Day");
+        else
+            System.out.println("Night");
+
     }
 }
