93701ffa0d891387a435080fef775b6817e721a8
Almas Sayem <mdassayem0@gmail.com>
2026-09-20 14:13:41 +0600

    updated

--- mini_project.java
+++ mini_project.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class mini_project {
-    
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int mynumber=(int)(Math.random()*100);
+       int usernumber=0;
+       do{
+        System.out.println("Guess my number:");
+        usernumber=sc.nextInt();
+        if(usernumber==mynumber){
+            System.out.println("Correct Number");
+            break;
+        }
+        else if(usernumber>mynumber){
+            System.out.println("Number too large");
+        }
+        else{
+           System.out.println("Number is smaller"); 
+        }
+       }while(usernumber>=0);
+       System.out.println("My number was:");
+       System.out.println(mynumber);
+    }
 }
