a054a46a2397cb027cb7b281bbb0297c22c7f9fc
Almas Sayem <mdassayem0@gmail.com>
2026-09-21 16:19:26 +0600

    updated

--- oops.java
+++ oops.java
@@ -1,16 +1,52 @@
 
-class pen{
+class Pen{
     String color;
     String type;
     public void write(){
         System.out.println("Write Something");
     }
+    public void printcolor(){
+        System.out.println(this.color);
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    public void printinfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+    Student(String name, int age){
+    this.name=name;
+    this.age=age;
+    }
+    public void printinfo(String name){
+        System.out.println(name);
+    }
+    public void printinfo(int age){
+        System.out.println(age);
+    }
+    public void printinfo(String name,int age){
+        System.out.println(name + " " + age);
+    }
 }
 public class oops {
     public static void main(String[] args) {
-        pen pen1=new pen();
+        Pen pen1=new Pen();
         pen1.color="Blue";
         pen1.type="Gel";
-        pen1.write();
+
+        Pen pen2=new Pen();
+        pen2.color="black";
+        pen2.type="ballpoint";
+        pen2.write();
+        
+        pen1.printcolor();
+        pen2.printcolor();
+
+        Student s1=new Student("Sayem",21);
+        s1.printinfo();
+        s1.printinfo(s1.name,s1.age);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,3 +1,1 @@
-   // System.out.println("Input your age: ");
-        // int age=sc.nextInt();
-        // System.out.println(age);
+
